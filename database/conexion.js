09d6b0875cb8a1b23cedc7a0cd89db101553ffc8
darwinestiven <dd5147609@gmail.com>
2023-12-28 17:16:18 -0500

    Commit Inicial

--- database/conexion.js
+++ database/conexion.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
-const db = new Sequelize("mascotas","mascotas","mascotas2023",{
+const db = new Sequelize("mascotas","mascotas","12345",{
     dialect: "mysql",
     host: "localhost"
 });
